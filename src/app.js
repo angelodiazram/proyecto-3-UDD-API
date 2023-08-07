cd5112e5e0c0c3ae141a77efd102dad7b7542cf7
Angelo Díaz R <angelodiazram@gmail.com>
2023-08-06 21:57:33 -0400

    agregado de header al diseño y botones del dashboard

--- src/app.js
+++ src/app.js
@@ -4,19 +4,20 @@ const inputPoke = document.querySelector('#inputPoke');
 const btn = document.querySelector('#btnPoke');
 const screen = document.querySelector('#screen');
 const ctx = document.getElementById('screenGraph');
+const pokeCardsContainer = document.querySelector('#pokeCards');
 
 let queryPokemon = '';
 
 // función asincrona que realiza la petición a la API
-const peticionApi = async() => {
+const peticionApi = async(pokemon) => {
     
-    // asignación del 'valor' a la constante proveniente del input
-    queryPokemon = inputPoke.value;
+    // asignación del 'valor' a la constante proveniente del div creado para los pokemones
+    queryPokemon = pokemon
     
     // End point de la API a consumir utilizando 'fetch'
-    const url = `https://pokeapi.co/api/v2/pokemon/${queryPokemon}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     const response = await fetch(url);
-    const dataPoke = await response.json()
+    const dataPoke = await response.json();
 
     // variables traidas desde el objeto de la API
     let pokeName = dataPoke.name;
@@ -31,7 +32,7 @@ const peticionApi = async() => {
     console.log(dataPoke);
 
     graphPoke(dataPoke);
-}
+};
 
 // Creación de grafico tipo radar
 const graphPoke = (pokemon) => {
@@ -61,12 +62,52 @@ const graphPoke = (pokemon) => {
             }]
         }
     });
-}
+};
 
-btn.addEventListener('click', async(e) => {
+// btn.addEventListener('click', async(e) => {
     
-    e.preventDefault()
+//     e.preventDefault()
     
-    peticionApi()
+//     peticionApi()
 
-})
+// })
+
+// función asincrona para hacer la petición a la API
+const getPokeList = async () => {
+
+    try {
+        // traemos la información desde la pokeAPI usando un fetch
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+        const data = await response.json();
+        // llamada a la función que crea los contenedores para la información recibida
+        createPokeContainers(data.results); //! data.results será "pokeList"
+    } catch (error){
+        console.log('Ha ocurrido el siguiente error:', error);
+    }
+};
+
+// función para crear los contenedores de la información obtenida de la petición
+const createPokeContainers = (pokeList) => {
+
+    // recorremos la lista para crear un contenedor por cada elemento
+    pokeList.forEach(poke => {
+        const pokeDiv = document.createElement('div');
+        pokeDiv.classList.add('pokemon');
+        pokeDiv.textContent = poke.name;
+        
+        // listener para ejecutar una la funcion de peticiónAPI al presionar el div
+        pokeDiv.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            // se ejecuta la función pasando como argumento el nombre del pokemon
+            // proveniente de la lista
+            peticionApi(poke.name);
+            console.log(poke.name);
+        });
+
+        // se agrega el div creado al contenedor principal
+        pokeCardsContainer.appendChild(pokeDiv);
+    });
+};
+
+getPokeList();
